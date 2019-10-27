a0786f079d34fab4d3c8f2b04bf5c5502220bfbd
Introback <57064513+Introback@users.noreply.github.com>
2019-10-28 00:47:30 +0300

    Update test3.py

--- test3.py
+++ test3.py
@@ -1,27 +1,9 @@
 import telebot
-import time
 
-TOKEN = '<775213554:AAE9878G2LmaEMoSavhy9J_ZE_oB0aK1SJw>'
+bot = telebot.Telebot("")
 
+@bot.message_handler(content_types=['text'])
+def send_echo(message):
+	bot.send_message(message.chat.id, message.text)
 
-def listener(messages):
-    """
-    When new messages arrive TeleBot will call this function.
-    """
-    for m in messages:
-        chatid = m.chat.id
-        if m.content_type == 'text':
-            text = m.text
-            tb.send_message(chatid, text)
-
-
-tb = telebot.TeleBot(TOKEN)
-tb.set_update_listener(listener) #register listener
-tb.polling()
-#Use none_stop flag let polling will not stop when get new message occur error.
-tb.polling(none_stop=True)
-# Interval setup. Sleep 3 secs between request new message.
-tb.polling(interval=3)
-
-while True: # Don't let the main Thread end.
-    pass
+bot.polling(none_stop = True)
