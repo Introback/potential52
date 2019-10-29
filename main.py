085b93582e8fbc9ad8103ee5f69c54d107a1aac9
Introback <57064513+Introback@users.noreply.github.com>
2019-10-29 04:44:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,9 @@
 import telebot
-import pyowm
+
 bot = telebot.TeleBot("775213554:AAE9878G2LmaEMoSavhy9J_ZE_oB0aK1SJw")
-owm = pyowm.OWM('6d00d1d4e704068d70191bad2673e0cc', language = "ru")
 
 @bot.message_handler(content_types=['text'])
 def send_echo(message):
-	observation = owm.weather_at_place(message.text)
-	w = observation.get_weather()
-	temp = w.get_temperature('celsius')["temp"]
-
-	answer = ("В городе " + message.text + " сейчас " + w.get_detailed_status() + ".\n")
-	answer += ("Температура сейчас в районе " + str(temp) + ".\n")
-
-	if temp < 10:
-		answer += ("Сейчас ппц как холодно, одевайся как танк!" + "\n")
-	elif temp < 20:
-		answer += ("Сейчас холодно, оденься потеплее." + "\n")
-	else:
-		answer += ("Температура норм, одевай что угодно." + "\n")
-
-	bot.send_message(message.chat.id, answer)
+	bot.send_message(message.chat.id, message.text)
 
 bot.polling(none_stop = True)
